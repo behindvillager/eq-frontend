2945d33a23a3accb3e90b23600e6b832d60a3f66
behindvillager <christian.hinterdorfer@behindvillager.com>
2025-12-08 07:17:33 +0000

    Rebrand from Home Assistant/Crush Assistant to eq cube

--- src/components/ha-sidebar.ts
+++ src/components/ha-sidebar.ts
@@ -386,7 +386,7 @@ class HaSidebar extends SubscribeMixin(LitElement) {
             ></ha-icon-button>
           `
         : ""}
-      <div class="title">Home Assistant</div>
+      <div class="title">eq cube</div>
     </div>`;
   }
 
--- src/components/ha-theme-picker.ts
+++ src/components/ha-theme-picker.ts
@@ -46,9 +46,7 @@ export class HaThemePicker extends LitElement {
           : nothing}
         ${this.includeDefault
           ? html`
-              <ha-list-item .value=${DEFAULT_THEME}>
-                Home Assistant
-              </ha-list-item>
+              <ha-list-item .value=${DEFAULT_THEME}> eq cube </ha-list-item>
             `
           : nothing}
         ${Object.keys(this.hass!.themes.themes)
